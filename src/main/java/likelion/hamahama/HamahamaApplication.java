0b8a02245119d8ff43c1c2e846cb93c0ff082051
helljh <junho308917@naver.com>
2023-08-18 05:28:05 +0900

    commit

--- src/main/java/likelion/hamahama/HamahamaApplication.java
+++ src/main/java/likelion/hamahama/HamahamaApplication.java
@@ -2,8 +2,10 @@ package likelion.hamahama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HamahamaApplication {
 
 	public static void main(String[] args) {
